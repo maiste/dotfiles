bdefda08830ea1b6dd22c6aadfffbb105b14e8fe
Etienne MARAIS <etienne@marais.green>
2019-06-20 11:20:40 +0200

    Add emacs configuration

---
+++ emacs/config/ocaml.el
@@ -0,0 +1,9 @@
+;; ##################
+;; # Kolibs - emacs #
+;; #================#
+;; #      OCaml     # 
+;; ##################
+
+;; merlin default config
+(require 'opam-user-setup "~/.emacs.d/config/opam-user-setup.el")
+
