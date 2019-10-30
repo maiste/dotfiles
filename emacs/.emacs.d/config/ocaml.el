d4656826763d2fb0e971ae8b3ce3422dae7f35d4
Etienne MARAIS <etiennemarais@maiste.fr>
2019-10-30 11:58:07 +0100

    remove emacs

--- emacs/.emacs.d/config/ocaml.el
+++
@@ -1,9 +0,0 @@
-;; ##################
-;; # Maiste - emacs #
-;; #================#
-;; #      OCaml     # 
-;; ##################
-
-;; merlin default config
-(require 'opam-user-setup "~/.emacs.d/config/opam-user-setup.el")
-
