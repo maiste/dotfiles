204548ef1eb1f282e2698815090bca602c964b13
Maiste <dev@maiste.fr>
2023-06-01 16:41:39 +0200

    nvim: support web helpers

--- editor/.config/nvim/lua/plugins/web.lua
+++ editor/.config/nvim/lua/plugins/web.lua
@@ -12,5 +12,46 @@ return {
         filetypes = { "html", "xml" }
       })
     end
-  }
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.diagnostics.eslint_d,
+          null_ls.builtins.diagnostics.zsh,
+          null_ls.builtins.formatting.prettier.with({
+            filetypes = {
+              "css",
+              "javascript",
+              "javascriptreact",
+              "typescript",
+              "typescriptreact",
+              "json",
+              "scss",
+              "less"
+            },
+          }),
+        }
+      })
+    end
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
+    },
+    config = function()
+      require("mason-null-ls").setup({
+        automatic_installation = true,
+      })
+    end,
+  },
 }
