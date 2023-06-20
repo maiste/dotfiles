0e16dfbee3bf16a3a01d73dbbfc436b6493ebede
Maiste <dev@maiste.fr>
2023-06-20 22:12:32 +0200

    nvim: improve markdown

--- editor/.config/nvim/lua/plugins/markdown.lua
+++ editor/.config/nvim/lua/plugins/markdown.lua
@@ -1,20 +1,25 @@
 return {
-  "toppair/peek.nvim",
-  build = "deno task --quiet build:fast",
-  ft = "markdown",
-  config = function()
-    require("peek").setup({
-      auto_load = false,
-      app = { "epiphany", "--private-instance" },
-    })
-  end,
-  keys = {
+  {
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    ft = "markdown",
+    config = function()
+      require("peek").setup({
+        auto_load = false,
+        app = { "epiphany", "--private-instance" },
+      })
+    end,
+    keys = {
+      {
+        "<leader>wp",
+        function()
+          require("peek").open()
+        end,
+        desc = "[w]riting [p]review"
+      }
+    },
     {
-      "<leader>wp",
-      function()
-        require("peek").open()
-      end,
-      desc = "[w]riting [p]review"
+      'jghauser/follow-md-links.nvim'
     }
   }
 }
