1d8cf3f2dc524387cb8362ccf5ab5aa034d6d140
Maiste <dev@maiste.fr>
2024-01-31 18:59:23 +0100

    nvim!: prepare new neovim config

---
+++ editor/.config/nvim.new/lua/core/package.lua
@@ -0,0 +1,30 @@
+-- +-------------------------------+
+-- | Neovim - Package Manager File |
+-- +-------------------------------+
+-- | Author: Maiste <dev@maiste.fr |
+-- | License: MIT                  |
+-- | Version: 20240131             |
+-- +-------------------------------+
+
+
+-- Install Lazy if not presents
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local ok, lazy = pcall(require, "lazy");
+if not ok then
+  print("Lazy not found")
+  return
+end
+
+lazy.setup({ spec = { import = 'plugins' }})
---
+++ editor/.config/nvim.new/lua/plugins/theme.lua
@@ -0,0 +1,12 @@
+-- +-------------------------------+
+-- | Neovim - Theme File           |
+-- +-------------------------------+
+-- | Author: Maiste <dev@maiste.fr |
+-- | License: MIT                  |
+-- | Version: 20240131             |
+-- +-------------------------------+
+
+return {
+  { "shaunsingh/nord.nvim"  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
