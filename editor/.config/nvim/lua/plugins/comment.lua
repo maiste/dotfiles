991e7ced0c39a6de628d2cadb5cf754cf13f6d7b
Etienne Marais <dev@maiste.fr>
2024-08-18 12:12:25 +0200

    nvim: add todo-comments plugin

--- editor/.config/nvim/lua/plugins/comment.lua
+++ editor/.config/nvim/lua/plugins/comment.lua
@@ -39,6 +39,13 @@ local specs = {
       end,
       "[c]omment [i]nvert")
   end,
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup()
+    end
+  }
 }
 
 return specs
